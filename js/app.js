9098a7ea7718d11f6bbcb5d47959b8b7c2e339e5
Oleksandr Dobrianskyi <dobrianskyialex@gmail.com>
2023-03-10 18:45:36 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', e => {
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
+
+const t = document.querySelector(`title`), w = 'dobrastro_ksenia', e = `❤❤❤❤❤❤`;
+let c = 0;
+setInterval(() => {
+	c = (c < w.length) ? c + 1 : 0;
+	t.textContent = w.slice(0, c);
+	if (c === w.length) setTimeout(() => t.textContent += ` ${e}`, 500);
+}, 500);
